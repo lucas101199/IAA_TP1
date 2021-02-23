2bc0868c006ee7e6c0e99e8a4d53ef4e20886bc3
lucas <lucas_fernandez@orange.fr>
2021-02-23 17:54:23 +0100

    Q2

--- knn.py
+++ knn.py
@@ -1,10 +1,12 @@
 import numpy as np
 from scipy.spatial import distance
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 
 """
+#Q1.1
 def array_center(X) :
     center = [0] * 0
     center.append(0.5 * np.ones(len(X[0])))
@@ -34,6 +36,8 @@ for d in range(1, 21):
     print(np.mean(dist), np.mean(v))
 """
 
+
+# Q1.2
 def damier(dimension, grid_size, nb_examples, noise=0) :
     data = np.random.rand(nb_examples, dimension)
     labels = np.ones(nb_examples)
@@ -47,6 +51,7 @@ def damier(dimension, grid_size, nb_examples, noise=0) :
     return data, labels
 
 
+"""
 noises = [0, 0.1, 0.2]
 score_max = 0
 dima = []
@@ -62,10 +67,10 @@ for k in range(1, 6) :
             for noise in noises :
                 for nbex in range(1000, 10000, 1000) :
                     X, y = damier(dim, nbcases, nbex, noise)
-                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-                    neigh = KNeighborsClassifier(n_neighbors=k)
-                    neigh.fit(X_train, y_train)
-                    score = neigh.score(X_test, y_test)
+                    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42) # separe les donnees en donnees train et test
+                    neigh = KNeighborsClassifier(n_neighbors=k) cree un classifier pour les KNN
+                    neigh.fit(X_train, y_train) # entraine le classifier avec les donnees d'entrainement
+                    score = neigh.score(X_test, y_test) recupere le score du classifier sur les donnees test
                     if score_max < score:
                         score_max = score
                         print(k)
@@ -106,3 +111,47 @@ plt.scatter(nbexa, scorea)
 plt.savefig('nb_ex.png')
 plt.show()
 
+best score k=4, dim=2, nbcase=2, noise=0, nbex=8000, score=0.99375
+
+
+#Q1.2.2
+X, y = damier(2, 2, 8000, 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+score_max = 0
+k_opt = 0
+for k in range(1, 6):
+    clf = KNeighborsClassifier(n_neighbors=k)
+    clf.fit(X_train, y_train)
+    scores = cross_val_score(clf, X_train, y_train, cv=5) #cross validation sur le classifier
+    if score_max < np.mean(scores):
+        score_max = np.mean(scores)
+        k_opt = k
+        print(k_opt)
+        print(score_max)
+
+clf = KNeighborsClassifier(n_neighbors=k_opt)
+clf.fit(X_train, y_train)
+print(clf.score(X_test, y_test))
+
+
+# Q1.2.3
+for grid in range(2, 11):
+    X, y = damier(2, grid, 1000, 0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    score_max = 0
+    k_opt = 0
+    for k in range(1, 6):
+        clf = KNeighborsClassifier(n_neighbors=k)
+        clf.fit(X_train, y_train)
+        scores = cross_val_score(clf, X_train, y_train, cv=5)
+        if score_max < np.mean(scores):
+            score_max = np.mean(scores)
+            k_opt = k
+    clf = KNeighborsClassifier(n_neighbors=k_opt)
+    clf.fit(X_train, y_train)
+    print("score = " + str(clf.score(X_test, y_test)))
+    print("grid size = " + str(grid))
+    print("k = " + str(k_opt))
+"""
+
+# Q1.2.4
